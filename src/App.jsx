745dd9881ad525253da6f3b9a1eeb222fff85f54
MrSetOne <lara.sanchez.michael@gmail.com>
2022-07-17 11:30:27 +0200

    blockRoute

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Map from "./components/Map/Map";
 import LogPage from "./components/LogPage/LogPage";
+import { useSelector } from "react-redux";
 import "./antd-theme/antd-customized.css";
 
 function App() {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/logpage" element={<LogPage />} />
-        <Route path="/map" element={<Map />} />
-      </Routes>
+      {user ? (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+      ) : (
+        <LogPage />
+      )}
     </div>
   );
 }
