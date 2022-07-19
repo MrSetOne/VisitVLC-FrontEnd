b5e058333db92d418d00ce60654a7ac29e19bfbc
MrSetOne <lara.sanchez.michael@gmail.com>
2022-07-19 18:33:11 +0200

    main menu basic + dropdown size fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Home.scss";
+import {
+  faUser,
+  faCrown,
+  faMapLocationDot,
+  faFilter,
+  faLocationPin,
+  faPlaceOfWorship,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Home = () => {
   return (
     <div className="Home">
-      <h1>ESTÁS EN EL HOME</h1>
+      <div className="Home__btns">
+        <button>
+          <FontAwesomeIcon icon={faUser} />
+          <p>My profile</p>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faCrown} />
+          <p> My favourite routes</p>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faLocationPin} />
+          <p> Close to me</p>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faMapLocationDot} />
+          <p>All routes</p>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faFilter} />
+          <p>Categories</p>
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faPlaceOfWorship} />
+          <p> Sites</p>
+        </button>
+      </div>
     </div>
   );
 };
