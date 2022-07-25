594a2e482c8f595c4c1fc325bc8d8a42e743cb78
Santiremix <santiagoguevara118@gmail.com>
2022-07-25 21:28:00 +0200

    Starting design showRoutes

--- src/components/ShowRoutes/ShowRoute/ShowRoute.jsx
+++ src/components/ShowRoutes/ShowRoute/ShowRoute.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './ShowRoute.scss'
 
 const ShowRoute = ({ route }) => {
   console.log(route);
@@ -7,9 +8,9 @@ const ShowRoute = ({ route }) => {
     <Link to={`/route/${route.route_id}`}>
       <div className="ShowRoute">
         <h2>{route.name}</h2>
-        <p>Difficult: {route.difficulty}</p>
-        <p>Points: {route.poi.length}</p>
-        <p>Duration: {route.duration}min.</p>
+        <p>Dificultad: {route.difficulty}</p>
+        <p>Puntos: {route.poi.length}</p>
+        <p>Duración: {route.duration}min.</p>
         <img src={route.image} alt={route.name} />
       </div>
     </Link>
