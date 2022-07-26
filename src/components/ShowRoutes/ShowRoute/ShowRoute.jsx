588860bc3a8f51b9c8889209a393788cab00f5ed
Santiremix <santiagoguevara118@gmail.com>
2022-07-26 11:19:38 +0200

    Navigate from allroutes to info working

--- src/components/ShowRoutes/ShowRoute/ShowRoute.jsx
+++ src/components/ShowRoutes/ShowRoute/ShowRoute.jsx
@@ -7,13 +7,14 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./ShowRoute.scss";
 
 const ShowRoute = ({ route }) => {
+  const navigate = useNavigate()
   console.log(route);
   return (
-    <button className="ShowRoute">
+    <button className="ShowRoute" onClick={() => navigate(`/route/${route.route_id}`)}>
       <h2>{route.name}</h2>
       <img src={route.image} alt={route.name} />
       <div>
